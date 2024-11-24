e3f1dea12998d27956b8103bde251b70752007ff
PATRICIA-COSMINA GABOR <patricia.gabor@stud.ubbcluj.ro>
2024-11-24 20:34:58 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GaborPatriciaLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
